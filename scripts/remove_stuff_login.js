3243aa54866c9ade71dcd057c15dc8094378a0a7
Jesse Wilson <jesse.wilson@qrious.co.nz>
2024-02-03 19:00:04 +1300

    feat: preemptively add empty script element to DOM

--- scripts/remove_stuff_login.js
+++ scripts/remove_stuff_login.js
@@ -1,5 +1,8 @@
-function handleClick(event) {
-    console.log("Click event");
-  }
-
-document.addEventListener("click", handleClick);
+// On Stuff.co.nz, the modal is inserted via a React hook.
+// This script pre-empts it by adding an empty 'piano-script' element to the DOM before the hook runs.
+document.addEventListener('DOMContentLoaded', function () {
+  const emptyScript = document.createElement('script');
+  emptyScript.id = 'piano-script';
+  emptyScript.style.display = 'none';
+  document.body.appendChild(emptyScript);
+});
