4ca433fc89ba2977fcef8cb8f6ba10635fd87715
Jesse Wilson <jesse.wilson@qrious.co.nz>
2024-02-03 20:51:00 +1300

    chore: update script comments

--- scripts/remove_stuff_login.js
+++ scripts/remove_stuff_login.js
@@ -1,5 +1,6 @@
-// On Stuff.co.nz, the modal is inserted via a React hook.
-// This script pre-empts it by adding an empty 'piano-script' element to the DOM before the hook runs.
+// On Stuff.co.nz, a script that contains the modal logic is inserted via a React hook.
+// The React hook checks for the existence of the modal script before inserting it.
+// This script pre-empts this by inserting an empty script tag with the same ID as the modal script.
 document.addEventListener('DOMContentLoaded', function () {
   const emptyScript = document.createElement('script');
   emptyScript.id = 'piano-script';
